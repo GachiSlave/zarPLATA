d4f1db9497bdddcd28bcfb2dfaab43d0399258a6
Vlad Lugovykh <71728038+GachiSlave@users.noreply.github.com>
2021-10-01 15:08:16 +0500

    Button2 task 2

--- Laba/main.c
+++ Laba/main.c
@@ -1,32 +1,41 @@
-/*
- * Copyright (C) 2015 Kaspar Schleiser <kaspar@schleiser.de>
- *
- * This file is subject to the terms and conditions of the GNU Lesser
- * General Public License v2.1. See the file LICENSE in the top level
- * directory for more details.
- */
-
-/**
- * @ingroup     examples
- * @{
- *
- * @file
- * @brief       example application for setting a periodic wakeup
- *
- * @author      Kaspar Schleiser <kaspar@schleiser.de>
- *
- * @}
- */
-
-#include <stdio.h>
 #include "xtimer.h"
 #include "timex.h"
 #include "periph/gpio.h"
 
-/* set interval to 1 second */
-#define INTERVAL (1U * US_PER_SEC * 0.1)
+
+// Обработчик прерывания по нажатию кнопки
+void btn_handler(void *arg)
+{
+  // Прием аргументов, передаваемых из главного потока.
+  (void)arg;
+  
+  // Переключение состояния пина PC8
+ 
+    gpio_toggle(GPIO_PIN(PORT_C, 8));
+    xtimer_sleep (0.1);
+
+  
+}
+
 
 int main(void)
+{
+  // Инициализация пина PA0, к которому подключена кнопка, как источника прерывания.
+  // GPIO_RISING - прерывание срабатывает по фронту
+  // btn_handler - имя функции обработчика прерывания
+  // NULL - ничего не передаем в аргументах
+
+    gpio_init_int(GPIO_PIN(PORT_A, 0), GPIO_IN_PD , GPIO_BOTH, btn_handler, NULL);
+  // Инициализация пина PC8 на выход
+
+  gpio_init(GPIO_PIN(PORT_C, 8), GPIO_OUT);
+
+  while(1){
+
+  }
+  return 0;
+}
+/* int main(void)
 {
     gpio_init(GPIO_PIN(PORT_C,8), GPIO_OUT);
     gpio_init(GPIO_PIN(PORT_C,9), GPIO_OUT);
@@ -41,5 +50,5 @@ int main(void)
     }
 
     return 0;
-}
-    
+    }
+    */
